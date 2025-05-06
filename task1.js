5a5dcfcd3257e9d45b48b86e850f05b6e3865fc0
SashOK <qa.shvets@gmail.com>
2025-05-06 11:55:09 +0300

    Виправлення

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
-let averageGrade = 91;
+let averageGrade = 60;
 if (averageGrade > 0 && averageGrade < 60) {
     console.log("Незадовільно");
-} else if (averageGrade > 60 && averageGrade <= 70) {
+} else if (averageGrade >= 60 && averageGrade <= 70) {
     console.log("Задовільно");
 } else if (averageGrade > 70 && averageGrade <= 80) {
     console.log("Добре");
